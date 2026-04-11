715321b3284efc58535071e14679cb253141e2be
Boros <420blazemachine420@gmail.com>
2026-04-11 00:01:25 -0700

    Inefficient but working

--- src/main/java/Schedule.java
+++ src/main/java/Schedule.java
@@ -1,41 +1,119 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+
 
 
 public class Schedule {
-    ArrayList<ArrayList<Job>> jobAdjacencyList;
-    ArrayList<Integer> inDegrees;
+    ArrayList<Job> jobList;
+	int completion;
     public Schedule(){
-        jobAdjacencyList = new ArrayList<>();
-        inDegrees = new ArrayList<>();
+		jobList = new ArrayList<>();
+		completion = 0;
     }
     public Job insert(int time){
-        ArrayList<Job> newJob = new ArrayList<>();
-        newJob.add(new Job(time));
-        jobAdjacencyList.add(newJob);
-        return get(jobAdjacencyList.size()-1);
+        jobList.add(new Job(time));
+		dag();
+        return get(jobList.size()-1);
     }
     public Job get(int index){
-        return jobAdjacencyList.get(index).get(0);
+        return jobList.get(index);
     }
     public int finish(){
-        //start topological sort using kahn's
-
+		return completion;
     }
+
+	private ArrayList<Job> kahn(){
+		HashMap<Job, Integer> inDeg = new HashMap<>();
+		ArrayList<Job> ordered = new ArrayList<>();
+
+		//initialize indegree list
+		for(int i = 0; i < jobList.size(); i++){
+			inDeg.put(jobList.get(i), 0);
+		}
+
+		//increment indegrees
+		for(int i = 0; i < jobList.size(); i++){
+			for(int j = 0; j < jobList.get(i).edgeList.size(); j++){
+				Job jobKey = jobList.get(i).edgeList.get(j);
+				inDeg.put(jobKey, inDeg.get(jobKey)+1);
+			}
+		}
+		//insert 0 indegree nodes first
+		for(int i = 0; i < jobList.size(); i++){
+			Job jobKey = jobList.get(i);
+			if(inDeg.get(jobKey) == 0){
+				inDeg.remove(jobKey);
+				ordered.add(jobKey);
+			}
+		}
+
+		//decrements indegrees and completes ordered list
+		for(int i = 0; i < ordered.size(); i++){
+			for(int j = 0; j < ordered.get(i).edgeList.size(); j++){
+				Job jobKey = ordered.get(i).edgeList.get(j);
+
+				Integer val = inDeg.put(jobKey, inDeg.get(jobKey)-1);
+				if(val-1 <= 0){
+					inDeg.remove(jobKey);
+					ordered.add(jobKey);
+				}
+			}
+		}
+		if(ordered.size() != jobList.size()){
+			for(Job job : inDeg.keySet()){
+				job.cycle = true;
+			}
+		}
+		return ordered;
+
+	}
+	private void dag(){
+		//start topological sort using kahn's
+		ArrayList<Job> schedList = kahn();
+
+		//reset all starting and finish times.
+		for(int i = 0; i < schedList.size(); i++){
+			schedList.get(i).resetTime();
+		}
+
+		int m = 0;
+		for(int i = 0; i < schedList.size(); i++){
+			Job u = schedList.get(i);
+			u.fTime = u.sTime + u.time;
+			for(int j = 0; j < schedList.get(i).edgeList.size(); j++){
+				Job v = schedList.get(i).edgeList.get(j);
+				v.sTime = Math.max(u.fTime, v.sTime);
+			}
+			m = Math.max(m, u.fTime);
+		}
+		completion = (schedList.size() != jobList.size())
+				? -1
+				: m;
+	}
 
     class Job{
         int sTime;
-        int cTime;
-        private Job(int cTime){
+        int fTime;
+		final int time;
+		boolean cycle;
+		ArrayList<Job> edgeList;
+        private Job(int time){
             sTime = 0;
-            this.cTime = cTime;
+            fTime = 0;
+			this.time = time;
+			cycle = false;
+			edgeList = new ArrayList<>();
         }
-        private void requires(Job j){
-            int i = 0;
-            while(jobAdjacencyList.get(i).get(0) != j){
-                i++;
-            }
-            jobAdjacencyList.get(i).add(this);
+        public void requires(Job j){
+            j.edgeList.add(this);
+			dag();
         }
-        public int start(){}
+        public int start(){
+			return (cycle) ? -1 : sTime;
+		 }
+		private void resetTime(){
+			sTime = 0;
+			fTime = time;
+		}
     }
 }
--- src/test/java/ScheduleTest.java
+++ src/test/java/ScheduleTest.java
@@ -10,25 +10,45 @@ public class ScheduleTest {
         schedule.insert(8); //adds job 0 with time 8
         Schedule.Job j1 = schedule.insert(3); //adds job 1 with time 3
         schedule.insert(5); //adds job 2 with time 5
-        schedule.finish(); //should return 8, since job 0 takes time 8 to complete.
-        /* Note it is not the earliest completion time of any job, but the earliest the entire set can complete. */
-        schedule.get(0).requires(schedule.get(2)); //job 2 must precede job 0
-        schedule.finish(); //should return 13 (job 0 cannot start until time 5)
-        schedule.get(0).requires(j1); //job 1 must precede job 0
-        schedule.finish(); //should return 13
-        schedule.get(0).start(); //should return 5
-        j1.start(); //should return 0
-        schedule.get(2).start(); //should return 0
-        j1.requires(schedule.get(2)); //job 2 must precede job 1
-        schedule.finish(); //should return 16
-        schedule.get(0).start(); //should return 8
-        schedule.get(1).start(); //should return 5
-        schedule.get(2).start(); //should return 0
-        schedule.get(1).requires(schedule.get(0)); //job 0 must precede job 1 (creates loop)
-        schedule.finish(); //should return -1
-        schedule.get(0).start(); //should return -1
-        schedule.get(1).start(); //should return -1
-        schedule.get(2).start(); //should return 0 (no loops in prerequisites)
+
+		//job 0 takes time 8 to complete.
+		assertEquals(8, schedule.finish());
+
+		//job 2 must precede job 0
+        schedule.get(0).requires(schedule.get(2));
+
+        //(job 0 cannot start until time 5)
+		assertEquals(13, schedule.finish());
+
+		//job 1 must precede job 0
+		schedule.get(0).requires(j1);
+
+		//should return 13
+		assertEquals(13, schedule.finish());
+
+		assertEquals(5, schedule.get(0).start());
+		assertEquals(0, j1.start());
+		assertEquals(0, schedule.get(2).start());
+
+		//job 2 must precede job 1
+        j1.requires(schedule.get(2));
+
+		assertEquals(16, schedule.finish());
+
+		assertEquals(8, schedule.get(0).start());
+		assertEquals(5, schedule.get(1).start());
+		assertEquals(0, schedule.get(2).start());
+
+		//job 0 must precede job 1 (creates loop)
+        schedule.get(1).requires(schedule.get(0));
+
+		assertEquals(-1, schedule.finish());
+
+		assertEquals(-1, schedule.get(0).start());
+		assertEquals(-1, schedule.get(1).start());
+		// (no loops in prerequisites)
+		assertEquals(0, schedule.get(2).start());
+
     }
 
 }
